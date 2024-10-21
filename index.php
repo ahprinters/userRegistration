f89740afa7e76b3d3f1d6453d4d1e83fc3682a71
ahprinters <ahps323@gmail.com>
2024-10-22 00:08:21 +0600

    jsonfile

--- index.php
+++ index.php
@@ -19,20 +19,20 @@
         </div>
 
         <!-- Display Registered Users -->
-        <h2>Registered Users</h2>
+        <!-- <h2>Registered Users</h2> -->
         <div>
             <?php
             // Read the JSON file
-            $jsonData = file_get_contents('data/users.json');
+            // $jsonData = file_get_contents('data/users.json');
 
             // Decode the JSON data into an associative array
-            $users = json_decode($jsonData, true);
+            // $users = json_decode($jsonData, true);
 
             // Display each user on a new line
-            foreach ($users as $username => $details) {
-                echo "Username: " . htmlspecialchars($username) . "<br>";
-                echo "Password: " . htmlspecialchars($details['password']) . "<br><br>";
-            }
+            // foreach ($users as $username => $details) {
+            //     echo "Username: " . htmlspecialchars($username) . "<br>";
+            //     echo "Password: " . htmlspecialchars($details['password']) . "<br><br>";
+            // }
             ?>
         </div>
 
